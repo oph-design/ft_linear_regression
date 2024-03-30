fa532da2dbdfc79ea76821b1ae2cf1a37d67ce57
oph-design <ole.paul.heinzelmann@gmail.com>
2024-03-30 20:43:22 +0100

    optimize train.py code

--- train.py
+++ train.py
@@ -4,15 +4,14 @@ import pandas as pd
 import time
 import matplotlib.pyplot as plt
 
-m: float = 0.0
-c: float = 0.0
+M: float = 0.0
+C: float = 0.0
 L: float = 0.0001
+Iterations: int = 1000
 
 
-def plot_data(data: pd.DataFrame):
+def plot_data(x, y):
     """plots the data from the csv"""
-    x = np.array(data.get("km"))
-    y = np.array(data.get("price"))
     plt.scatter(x, y)
     plt.xlabel("mileage")
     plt.ylabel("price")
@@ -20,48 +19,38 @@ def plot_data(data: pd.DataFrame):
     plt.show()
 
 
-def predict_price(mileage: float) -> float:
-    """predict function for the price"""
-    return c + m * mileage
-
-
-def calc_error(mileage: float, price: float, var: bool) -> float:
-    """calculates single error based on the varibale"""
-    if var is True:
-        return mileage * (price - predict_price(mileage))
-    return price - predict_price(mileage)
-
-
-def calc_loss(data: pd.DataFrame, var: bool) -> float:
+def calc_loss(x, y, var) -> float:
     """calculates loss dependent on the variable"""
     sum: float = 0.0
-    for index, row in data.iterrows():
-        sum += calc_error(float(row["km"]), float(row["price"]), var)
-    return -2 * float(len(data)) * sum
+    for i in x:
+        predicted_price = C + M * x[i]
+        error = y[i] - predicted_price
+        if var is True:
+            error *= y[i]
+        sum += error
+    return (-2 / len(x)) * sum
 
 
-def train_model(data: pd.DataFrame):
+def train_model(x, y):
     """trains the model with gradient descent"""
-    loss_c = calc_loss(data, False)
-    loss_m = calc_loss(data, True)
-    for i in range(1000):
-        global c, m
-        c -= L * loss_c
-        m -= L * loss_m
-        loss_m = calc_loss(data, False)
-        loss_c = calc_loss(data, True)
-        print(f"c: {c} loss: {loss_c} m:{m} loss: {loss_m}")
+    for i in range(Iterations):
+        global C, M
+        loss_m = calc_loss(x, y, True)
+        loss_c = calc_loss(x, y, False)
+        C -= L * loss_c
+        M -= L * loss_m
+        print(f"c: {C} loss: {loss_c} m:{M} loss: {loss_m}")
         time.sleep(1)
 
 
 def main():
     """main function"""
     data = pd.read_csv("data.csv")
-    print(len(data))
-    # print(data.describe())
-    # plot_data(data)
-    train_model(data)
-    print(f"final formular: price = {c} + {m} * mileage")
+    x = np.array(data.get("km"))
+    y = np.array(data.get("price"))
+    # plot_data(x, y)
+    train_model(x, y)
+    print(f"final formular: price = {C} + {M} * mileage")
 
 
 if __name__ == "__main__":

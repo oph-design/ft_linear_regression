4fc6704a50a3361640508a30bbffd67e44593ff6
oph-design <ole.paul.heinzelmann@gmail.com>
2024-03-31 12:05:24 +0200

    implement denormilization

--- train.py
+++ train.py
@@ -19,23 +19,19 @@ def plot_data(x, y):
     plt.show()
 
 
-def calc_loss(x, y, length, var) -> float:
+def calc_loss(x, y, length, slope) -> float:
     """calculates loss dependent on the variable"""
-    sum: float = 0.0
-    for i in range(length):
-        predicted_price = C + M * x[i]
-        error = predicted_price - y[i]
-        if var is True:
-            error = error * x[i]
-        sum = sum + error
-    return (2 / length) * sum
+    prediction = C + M * x
+    if slope is False:
+        return (2 / length) * sum(prediction - y)
+    return (2 / length) * sum(x * (prediction - y))
 
 
 def train_model(x, y):
     """trains the model with gradient descent"""
+    global C, M
     length = len(x)
     for i in range(Iterations):
-        global C, M
         loss_c = calc_loss(x, y, length, False)
         loss_m = calc_loss(x, y, length, True)
         if i % 100 == 0:
@@ -44,19 +40,29 @@ def train_model(x, y):
         M = M - L * loss_m
 
 
-def norm_data(arr):
+def normed(arr):
+    """normes array to 0 1 scale"""
     span = np.max(arr) - np.min(arr)
     return (arr - np.min(arr)) / span
+
+
+def denorm_coefs(x, y):
+    """transforms coefficiants to original scale"""
+    global M, C
+    spanx = np.max(x) - np.min(x)
+    spany = np.max(y) - np.min(y)
+    M = spany * M / spanx
+    C = np.min(y) + spany * C + M * (1 - np.min(x))
 
 
 def main():
     """main function"""
     data = pd.read_csv("data.csv")
-    x = norm_data(data.iloc[:, 0])
-    y = norm_data(data.iloc[:, 1])
-    train_model(x, y)
+    x = data.iloc[:, 0]
+    y = data.iloc[:, 1]
+    train_model(normed(x), normed(y))
+    denorm_coefs(x, y)
     plot_data(x, y)
-    print(f"final formular: price = {C} + {M} * mileage")
 
 
 if __name__ == "__main__":
